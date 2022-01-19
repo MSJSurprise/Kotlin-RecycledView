56181dc60ca0bf9b55f58179334feddcd3cfbb04
Surprise <mesanwoolasurprise303>
2022-01-19 00:00:03 -0800

    Created a class and a function

--- app/src/main/java/com/example/kotlinrecycledview/MainActivity.kt
+++ app/src/main/java/com/example/kotlinrecycledview/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.kotlinrecycledview
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvContacts: RecyclerView = findViewById(R.id.rvContacts)
+
+        rvContacts.setBackgroundColor(Color.RED)
+
+        val contacts = createContacts()
+    }
+
+    private fun createContacts(): List<Contact> {
+        val contacts = mutableListOf<Contact>()
+        for (i in 1..150) contacts.add(Contact("Person #$i", i))
+        return contacts
+
     }
 }
